189fb8ea825bc27867121cfc2ca0fc6d92ee8213
noodaj <jadon.loi.68@gmail.com>
2024-03-13 22:07:44 -0700

    feat: added OAuth login functionality

--- frontend/app/(auths)/login.tsx
+++ frontend/app/(auths)/login.tsx
@@ -1,10 +1,12 @@
-import { TouchableOpacity, View } from "react-native";
-import React, { useCallback } from "react";
-import { Button, Input, Text, TextArea, YStack } from "tamagui";
 import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
+import { useAuth, useOAuth, useUser } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
-import { useOAuth } from "@clerk/clerk-expo";
 import { useRouter } from "expo-router";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import React, { useCallback, useEffect } from "react";
+import { View } from "react-native";
+import { Button, Input, Separator, Text, XStack, YStack } from "tamagui";
+import { app } from "../firebaseConfig";
 
 enum Strategies {
   Google = "oauth-google",
@@ -13,16 +15,33 @@ enum Strategies {
 const Login = () => {
   useWarmUpBrowser();
 
-  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+  const { isSignedIn, user } = useUser();
+  const db = getFirestore(app);
   const router = useRouter();
 
-  const onPress = useCallback(async () => {
+  const { startOAuthFlow: gOAuth } = useOAuth({ strategy: "oauth_google" });
+  const { startOAuthFlow: aOAuth } = useOAuth({ strategy: "oauth_apple" });
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/");
+    }
+  }, [isSignedIn]);
+
+  const onPress = useCallback(async (strategy: Strategies) => {
     try {
-      const { createdSessionId, signIn, signUp, setActive } =
-        await startOAuthFlow();
+      const curAuth = {
+        [Strategies.Apple]: aOAuth,
+        [Strategies.Google]: gOAuth,
+      }[strategy];
+
+      const { createdSessionId, signIn, signUp, setActive } = await curAuth();
 
       if (createdSessionId) {
         setActive!({ session: createdSessionId });
+
+        collection(db, "users");
+
         router.back();
       }
     } catch (err) {
@@ -31,7 +50,7 @@ const Login = () => {
   }, []);
   return (
     <View>
-      <YStack>
+      <YStack gap={"$1"} paddingVertical={"$2"} marginHorizontal={15}>
         <Input
           size="$4"
           borderWidth={2}
@@ -40,24 +59,36 @@ const Login = () => {
         />
         <Input
           size="$4"
-          borderWidth={2}
+          borderWidth={4}
           placeholder="Password"
           autoCapitalize="none"
           secureTextEntry={true}
         />
-        <Button backgroundColor={"cyan"} mx={"$4"}>
-          <Text color={"$black2"}>Continue</Text>
+        <Button backgroundColor={"$red9"} mx={"$1"}>
+          <Text>Continue</Text>
         </Button>
       </YStack>
-
-      <Button
-        backgroundColor={"$black025"}
-        mx={"$4"}
-        icon={<Ionicons name="logo-google" size={24} />}
-        onPress={onPress}
-      >
-        Continue with Google
-      </Button>
+      <XStack paddingVertical={"$5"}>
+        <Separator alignSelf="stretch" />
+        <Text alignContent="center">Or</Text>
+        <Separator alignSelf="stretch" />
+      </XStack>
+      <YStack gap={"$1"} rowGap={"$2"}>
+        <Button
+          mx={"$4"}
+          icon={<Ionicons name="logo-google" size={24} />}
+          onPress={() => onPress(Strategies.Google)}
+        >
+          Continue with Google
+        </Button>
+        <Button
+          mx={"$4"}
+          icon={<Ionicons name="logo-apple" size={24} />}
+          onPress={() => onPress(Strategies.Apple)}
+        >
+          Continue with Apple
+        </Button>
+      </YStack>
       <YStack></YStack>
     </View>
   );

7b5d208bc40430e7b56808e8f651db7a5bd93652
noodaj <jadon.loi.68@gmail.com>
2024-03-13 22:09:32 -0700

    added database query for testing

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,6 +1,31 @@
-import { Text, View } from "@/components/Themed";
-import { Link } from "expo-router";
+import { useUser } from "@clerk/clerk-expo";
+import { collection, getDocs } from "firebase/firestore";
+import { SafeAreaView } from "react-native";
+import { Text, View } from "tamagui";
+import { db } from "../firebaseConfig";
+import { useEffect } from "react";
 
 export default function Index() {
-  return <View></View>;
+  const { user } = useUser();
+
+  const users = collection(db, "users");
+
+  const getUsers = async () => {
+    try {
+      const userSnapshot = await getDocs(users);
+
+      userSnapshot.forEach((item) => console.log(item.data(), item.id));
+    } catch (err) {}
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>{`hello ${user?.username ? user.username : user?.firstName}`}</Text>
+      </View>
+    </SafeAreaView>
+  );
 }

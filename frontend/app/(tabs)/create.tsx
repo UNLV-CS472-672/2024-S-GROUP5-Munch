66e09cd83cbadfd0c81bd484639b2cfadd4630f4
Michelle McGowan <mcgowm1@unlv.nevada.edu>
2024-03-17 20:23:11 -0700

    Updated UI
    - used stack to make it more uniform
    - centered the header

--- frontend/app/(tabs)/create.tsx
+++ frontend/app/(tabs)/create.tsx
@@ -2,33 +2,31 @@ import { Text, View } from "@/components/Themed";
 import React from "react";
 import { TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Button, H1, H2, H4 } from "tamagui";
+import { Button, H1, H2, H4, YStack } from "tamagui";
 
 // should open up gallery so people can upload vid/images
 // have option to just do text so new recipe
 
-export default function TabTwoScreen() {
+export default function Create() {
   return (
-    <SafeAreaView>
-      <View>
-        <H4>NEW POST</H4>
-        <View>
-          <Button></Button>
-          <TextInput
-            placeholder={"Hungry?"}
-            multiline={true}
-            style={{
-              height: 130,
-              borderRadius: 5,
-              paddingHorizontal: 10,
-              textAlignVertical: 'top'
-            }
-            }
-          />
-
-
-        </View>
-      </View>
-    </SafeAreaView>
+    <View>
+      <YStack>
+        <H4 style={{alignSelf: "center"}}>NEW POST</H4>
+        <TextInput
+          placeholder={"Hungry?"}
+          multiline={true}
+          style={{
+            height: 130,
+            borderRadius: 5,
+            paddingHorizontal: 10,
+            textAlignVertical: 'top'
+          }
+          }
+        />
+        <Button backgroundColor={'cyan'} mx={'$4'}>
+          <Text color={'$black2'}>Post</Text>
+        </Button>
+      </YStack>
+    </View>
   );
 }

bf728a6fa6c457fba93a9f6b208267998039c2db
Michelle McGowan <mcgowm1@unlv.nevada.edu>
2024-03-19 18:01:11 -0700

    Fixing Error
    - updated importing text
    - no more error

--- frontend/app/(tabs)/create.tsx
+++ frontend/app/(tabs)/create.tsx
@@ -1,8 +1,8 @@
-import { Text, View } from '@/components/Themed';
+import { View } from '@/components/Themed';
 import React from 'react';
 import { TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button, H1, H2, H4, YStack } from 'tamagui';
+import { Button, H1, H2, H4, YStack, Text } from 'tamagui';
 
 // should open up gallery so people can upload vid/images
 // have option to just do text so new recipe

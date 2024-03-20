4bbf1009585724e91f2f54cff4a591b40ce29926
noodaj <jadon.loi.68@gmail.com>
2024-03-20 12:54:29 -0700

    feat: added zod validation for user

--- frontend/types/user.ts
+++ frontend/types/user.ts
@@ -1,5 +1,46 @@
+import { z } from "zod";
 export type UserState = {
   username: string;
   firstName: string;
   lastName: string;
 };
+
+//registration
+export const RegisterSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be 3 characters long." })
+    .max(10, { message: "Username must be 10 characters long." }),
+  password: z
+    .string()
+    .refine(
+      (val) =>
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/.test(
+          val,
+        ),
+      "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character.",
+    ),
+  email: z.string().email(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+});
+
+export type RegisterSchemaInputs = z.infer<typeof RegisterSchema>;
+
+//login
+const loginSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be 3 characters long." }),
+  password: z
+    .string()
+    .refine(
+      (val) =>
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/.test(
+          val,
+        ),
+      "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character.",
+    ),
+});
+
+export type LoginSchema = z.infer<typeof loginSchema>;

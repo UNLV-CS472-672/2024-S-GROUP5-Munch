8fa8f37a024129198003b9561a034777f78dfd25
Michael Razon <ocmichaelrazon@gmail.com>
2024-03-21 23:33:01 -0700

    Resolved homepage coding style and added heart asset
    Resolved most of the coding style conflicts in (tabs)/index.tsx. I had to still use <StyledBG> because it was breaking with the standard method. I also added the heart asset used into the assets directory

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,45 +1,34 @@
-import { Text, View } from '@/components/Themed';
+/*  Homepage layout
+*   Includes a background image of a recipe image
+*   Includes a side bar with an Avatar, like button, comment
+*   button, and a share button. 
+*/
+
+
 import { Link } from 'expo-router';
 import { TouchableHighlight, ImageBackground } from 'react-native';
-import { Button, YStack, Image, styled, Avatar } from 'tamagui';
+import { Button, YStack, Image, styled, Avatar, Text, View } from 'tamagui';
 
 export default function Index() {
-  const StyledYStack = styled(YStack, {
-    backgroundColor: 'grey',
-    padding: 0,
-    width: 70,
-    height: 240,
-    x: 338,
-    y: 300,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  });
-  const StyledView = styled(View, { flex: 1 });
-  const backgroundImage = { uri: 'https://picsum.photos/1080/1920' };
-  const StyledBG = styled(ImageBackground, { flex: 1 });
+  const backgroundImage = {uri:'https://picsum.photos/1080/1920'}
+  const StyledBG = styled(ImageBackground, {flex:1})
   return (
-    <StyledView>
-      <StyledBG resizeMode='contain' source={backgroundImage}>
-        <Text>homepage</Text>
-        <StyledYStack>
-          <Avatar circular size='$6'>
-            <Avatar.Image src='https://picsum.photos/300/300' />
-          </Avatar>
-          <TouchableHighlight
-            onPress={() => console.log('Button pressed')}
-            underlayColor='crimson'
-            activeOpacity={1}
-          >
-            <Image source={require('./heart.png')} />
-          </TouchableHighlight>
-          <Button size='$5' circular>
-            comm
-          </Button>
-          <Button size='$5' circular>
-            share
-          </Button>
-        </StyledYStack>
-      </StyledBG>
-    </StyledView>
+    <View flex={1}>
+        <StyledBG resizeMode='contain' source={backgroundImage}>
+            <Text>homepage</Text>
+            <YStack backgroundColor={'grey'} width={70} height={240} x={338} y={300}
+                    justifyContent={'space-around'} alignItems={'center'}>
+                <Avatar circular size = "$6">
+                    <Avatar.Image src="https://picsum.photos/300/300"/>
+                </Avatar>
+                <TouchableHighlight onPress={() => console.log("Button pressed")}
+                                    underlayColor='crimson' activeOpacity={1} >
+                    <Image source={require('../../assets/images/heart.png')}/>
+                </TouchableHighlight>
+                <Button size="$5" circular>comm</Button>
+                <Button size="$5" circular>share</Button>
+            </YStack>
+       </StyledBG>
+    </View>
   );
 }

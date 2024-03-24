c3c8dc90fa1208400d4ebc21d600ddca29121062
Michelle McGowan <mcgowm1@unlv.nevada.edu>
2024-03-24 10:25:29 -0700

    Upload Picture
    - grant permissions to photo library

--- frontend/app/(tabs)/create.tsx
+++ frontend/app/(tabs)/create.tsx
@@ -2,13 +2,34 @@ import { View } from '@/components/Themed';
 import React, { useState } from 'react';
 import { TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button, H1, H2, H4, YStack, Text, Switch, XStack } from 'tamagui';
+import * as ImagePicker from "expo-image-picker";
+import { Button, H1, H2, H4, YStack, Text, Switch, XStack, AlertDialog, AlertDialogTrigger, AlertDialogTitle, AlertDialogDescription, Image } from 'tamagui';
 
 // should open up gallery so people can upload vid/images
 // have option to just do text so new recipe
 
 export default function Create() {
   const [isEnabled, setEnabledElements] = useState(false);
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const pickImg = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status !== "granted") {
+      <AlertDialog>
+        <AlertDialogTitle>No Permission Granted</AlertDialogTitle>
+        <AlertDialogDescription>Please allow access to upload image.</AlertDialogDescription>
+      </AlertDialog>
+    } else {
+      const result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) {
+        setFile(result.uri);
+        setError(null);
+      }
+    }
+  }
 
   return (
     <View>
@@ -20,9 +41,24 @@ export default function Create() {
             <Switch.Thumb animation="bouncy" />
           </Switch>
         </XStack>
+        <YStack>
+          <Text>Add Image</Text>
+          <Button onPress={pickImg}></Button>
+          {
+            file ? (
+
+              <Image
+                source={{
+                  uri: file
+                }}
+              />
+
+            ) : null
+          }
+        </YStack>
         {
           !isEnabled ? (
-            <YStack>          
+            <YStack>
               <TextInput
                 placeholder={'Description...'}
                 multiline={true}

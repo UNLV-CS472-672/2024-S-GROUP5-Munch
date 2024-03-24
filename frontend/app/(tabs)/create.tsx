b1a25e9265884a4f2e90adb4fa5ff4ee25818e03
Michelle McGowan <mcgowm1@unlv.nevada.edu>
2024-03-24 09:58:53 -0700

    Refactor Create Post
    - no more tabs
    - switch button to show or not show according to type of post
    - check the switch if it is a recipe

--- frontend/app/(tabs)/create.tsx
+++ frontend/app/(tabs)/create.tsx
@@ -1,27 +1,85 @@
 import { View } from '@/components/Themed';
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button, H1, H2, H4, YStack, Text } from 'tamagui';
+import { Button, H1, H2, H4, YStack, Text, Switch, XStack } from 'tamagui';
 
 // should open up gallery so people can upload vid/images
 // have option to just do text so new recipe
 
 export default function Create() {
+  const [isEnabled, setEnabledElements] = useState(false);
+
   return (
     <View>
       <YStack>
         <H4 style={{ alignSelf: 'center' }}>NEW POST</H4>
-        <TextInput
-          placeholder={'Hungry?'}
-          multiline={true}
-          style={{
-            height: 130,
-            borderRadius: 5,
-            paddingHorizontal: 10,
-            textAlignVertical: 'top',
-          }}
-        />
+        <XStack>
+          <Text fontSize="$5" paddingStart="$5">is this a recipe?</Text>
+          <Switch marginLeft="$2" size="$3" onCheckedChange={() => setEnabledElements(!isEnabled)}>
+            <Switch.Thumb animation="bouncy" />
+          </Switch>
+        </XStack>
+        {
+          !isEnabled ? (
+            <YStack>          
+              <TextInput
+                placeholder={'Description...'}
+                multiline={true}
+                style={{
+                  height: 130,
+                  borderRadius: 5,
+                  paddingHorizontal: 10,
+                  textAlignVertical: 'top',
+                }}
+              />
+              <TextInput
+                placeholder={'List Steps...'}
+                multiline={true}
+                style={{
+                  height: 130,
+                  borderRadius: 5,
+                  paddingHorizontal: 10,
+                  textAlignVertical: 'top',
+                }}
+              />
+              <TextInput
+                placeholder={'List Ingredients...'}
+                multiline={true}
+                style={{
+                  height: 130,
+                  borderRadius: 5,
+                  paddingHorizontal: 10,
+                  textAlignVertical: 'top',
+                }}
+              />
+            </YStack>
+          ) : null
+        }
+        {
+          isEnabled ? (
+            <YStack>
+              <TextInput
+                placeholder={'Upload IMG...'}
+                multiline={true}
+                style={{
+                  height: 130,
+                  borderRadius: 5,
+                  paddingHorizontal: 10,
+                  textAlignVertical: 'top',
+                }} />
+              <TextInput
+                placeholder={'Caption...'}
+                multiline={true}
+                style={{
+                  height: 130,
+                  borderRadius: 5,
+                  paddingHorizontal: 10,
+                  textAlignVertical: 'top',
+                }}
+              />
+            </YStack>) : null
+        }
         <Button backgroundColor={'cyan'} mx={'$4'}>
           <Text color={'$black2'}>Post</Text>
         </Button>

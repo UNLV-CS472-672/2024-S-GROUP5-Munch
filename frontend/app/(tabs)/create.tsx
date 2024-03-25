e71f92a34cafade71425935dbe632a596bbf2bec
mitshelle <mitshelle@users.noreply.github.com>
2024-03-25 20:58:46 +0000

    Apply formatting changes

--- frontend/app/(tabs)/create.tsx
+++ frontend/app/(tabs)/create.tsx
@@ -14,9 +14,8 @@ import {
   H5,
   Text,
   TextArea,
-  Switch
+  Switch,
 } from 'tamagui';
-
 
 export default function Create() {
   const [isEnabled, setEnabledElements] = useState(false);
@@ -26,71 +25,75 @@ export default function Create() {
       <YStack>
         <H4 style={{ alignSelf: 'center', color: 'black' }}>NEW POST</H4>
         <XStack>
-          <Text color={'black'} fontSize="$5" paddingStart="$5">is this a recipe?</Text>
-          <Switch marginLeft="$2" size="$3" onCheckedChange={() => setEnabledElements(!isEnabled)}>
-            <Switch.Thumb animation="bouncy" />
+          <Text color={'black'} fontSize='$5' paddingStart='$5'>
+            is this a recipe?
+          </Text>
+          <Switch
+            marginLeft='$2'
+            size='$3'
+            onCheckedChange={() => setEnabledElements(!isEnabled)}
+          >
+            <Switch.Thumb animation='bouncy' />
           </Switch>
         </XStack>
-        {
-          !isEnabled ? (
-            <YStack>          
-              <TextArea
-                placeholder={'Description...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }}
-              />
-              <TextArea
-                placeholder={'List Steps...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }}
-              />
-              <TextArea
-                placeholder={'List Ingredients...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }}
-              />
-            </YStack>
-          ) : null
-        }
-        {
-          isEnabled ? (
-            <YStack>
-              <TextArea
-                placeholder={'Upload IMG...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }} />
-              <TextArea
-                placeholder={'Caption...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }}
-              />
-            </YStack>) : null
-        }
+        {!isEnabled ? (
+          <YStack>
+            <TextArea
+              placeholder={'Description...'}
+              multiline={true}
+              style={{
+                height: 130,
+                borderRadius: 5,
+                paddingHorizontal: 10,
+                textAlignVertical: 'top',
+              }}
+            />
+            <TextArea
+              placeholder={'List Steps...'}
+              multiline={true}
+              style={{
+                height: 130,
+                borderRadius: 5,
+                paddingHorizontal: 10,
+                textAlignVertical: 'top',
+              }}
+            />
+            <TextArea
+              placeholder={'List Ingredients...'}
+              multiline={true}
+              style={{
+                height: 130,
+                borderRadius: 5,
+                paddingHorizontal: 10,
+                textAlignVertical: 'top',
+              }}
+            />
+          </YStack>
+        ) : null}
+        {isEnabled ? (
+          <YStack>
+            <TextArea
+              placeholder={'Upload IMG...'}
+              multiline={true}
+              style={{
+                height: 130,
+                borderRadius: 5,
+                paddingHorizontal: 10,
+                textAlignVertical: 'top',
+              }}
+            />
+            <TextArea
+              placeholder={'Caption...'}
+              multiline={true}
+              style={{
+                height: 130,
+                borderRadius: 5,
+                paddingHorizontal: 10,
+                textAlignVertical: 'top',
+              }}
+            />
+          </YStack>
+        ) : null}
         <Button backgroundColor={'cyan'} mx={'$4'}>
           <Text color={'$black2'}>Post</Text>
         </Button>

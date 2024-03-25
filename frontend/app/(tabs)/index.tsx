ebf651fc39611d2ceb7f4338cf5d25b0db4afaaf
Michael Razon <ocmichaelrazon@gmail.com>
2024-03-25 16:22:04 -0700

    Fixed background image
    Fixed background image to use Tamagui component. Also added a scrolling feature that will be used for posts in the future

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -5,43 +5,48 @@
  */
 
 import { Link } from 'expo-router';
-import { TouchableHighlight, ImageBackground } from 'react-native';
-import { Button, YStack, Image, styled, Avatar, Text, View } from 'tamagui';
+import { TouchableHighlight, ImageBackground, Dimensions } from 'react-native';
+import { Button, YStack, Image, styled, Avatar, Text, View, ScrollView } from 'tamagui';
 
 export default function Index() {
   const backgroundImage = { uri: 'https://picsum.photos/1080/1920' };
-  const StyledBG = styled(ImageBackground, { flex: 1 });
+  const windowWidth = Dimensions.get('window').width
+  const windowHeight = Dimensions.get('window').height
   return (
-    <View flex={1}>
-      <StyledBG resizeMode='contain' source={backgroundImage}>
-        <Text>homepage</Text>
-        <YStack
-          backgroundColor={'grey'}
-          width={70}
-          height={240}
-          x={338}
-          y={300}
-          justifyContent={'space-around'}
-          alignItems={'center'}
-        >
-          <Avatar circular size='$6'>
-            <Avatar.Image src='https://picsum.photos/300/300' />
-          </Avatar>
-          <TouchableHighlight
-            onPress={() => console.log('Button pressed')}
-            underlayColor='crimson'
-            activeOpacity={1}
-          >
-            <Image source={require('../../assets/images/heart.png')} />
-          </TouchableHighlight>
-          <Button size='$5' circular>
-            comm
-          </Button>
-          <Button size='$5' circular>
-            share
-          </Button>
-        </YStack>
-      </StyledBG>
-    </View>
+      <ScrollView>
+          <View flexDirection={'row-reverse'} width={windowWidth} height={windowHeight}
+                verticalPadding={60}>
+            <Image source={backgroundImage}
+                 width={windowWidth} height={windowHeight-100}
+                 resizeImage={'contain'}
+                 position={'absolute'}
+            />
+            <YStack
+              backgroundColor={'grey'}
+              width={70}
+              height={240}
+              y={300}
+              justifyContent={'space-around'}
+              alignItems={'center'}
+            >
+              <Avatar circular size='$6'>
+                <Avatar.Image src='https://picsum.photos/300/300' />
+              </Avatar>
+              <TouchableHighlight
+                onPress={() => console.log('Button pressed')}
+                underlayColor='crimson'
+                activeOpacity={1}
+              >
+                <Image source={require('../../assets/images/heart.png')} />
+              </TouchableHighlight>
+              <Button size='$5' circular>
+                comm
+              </Button>
+              <Button size='$5' circular>
+                share
+              </Button>
+            </YStack>
+          </View>
+      </ScrollView>
   );
 }

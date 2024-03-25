8ab68ce88aa1505d6ce026d1f8e6ba954e0881e4
Michelle McGowan <mcgowm1@unlv.nevada.edu>
2024-03-25 11:48:01 -0700

    Image Upload
    - functionality to upload image
    - uses uri from camera roll
    - shows image if there is one

--- frontend/app/(tabs)/create.tsx
+++ frontend/app/(tabs)/create.tsx
@@ -1,9 +1,9 @@
 import { View } from '@/components/Themed';
 import React, { useState } from 'react';
-import { TextInput } from 'react-native';
+
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as ImagePicker from "expo-image-picker";
-import { Button, H1, H2, H4, YStack, Text, Switch, XStack, AlertDialog, AlertDialogTrigger, AlertDialogTitle, AlertDialogDescription, Image } from 'tamagui';
+import { Button, H1, H2, H4, YStack, Text, TextArea, Switch, XStack, AlertDialog, AlertDialogTrigger, AlertDialogTitle, AlertDialogDescription, Image, ScrollView } from 'tamagui';
 
 // should open up gallery so people can upload vid/images
 // have option to just do text so new recipe
@@ -23,103 +23,95 @@ export default function Create() {
       </AlertDialog>
     } else {
       const result = await ImagePicker.launchImageLibraryAsync();
-
+      var uri = '';
+      result.assets.map((asset) => uri = asset.uri);
       if (!result.canceled) {
-        setFile(result.uri);
+        setFile(uri);       
         setError(null);
       }
     }
   }
 
   return (
-    <View>
-      <YStack>
-        <H4 style={{ alignSelf: 'center' }}>NEW POST</H4>
-        <XStack>
-          <Text fontSize="$5" paddingStart="$5">is this a recipe?</Text>
-          <Switch marginLeft="$2" size="$3" onCheckedChange={() => setEnabledElements(!isEnabled)}>
-            <Switch.Thumb animation="bouncy" />
-          </Switch>
-        </XStack>
+    <SafeAreaView>
+      <ScrollView>
         <YStack>
-          <Text>Add Image</Text>
-          <Button onPress={pickImg}></Button>
-          {
-            file ? (
-
-              <Image
-                source={{
-                  uri: file
-                }}
-              />
-
+          <H4 style={{ alignSelf: 'center' }}>NEW POST</H4>
+          <XStack>
+            <Text fontSize="$5" paddingStart="$5">is this a recipe?</Text>
+            <Switch marginLeft="$2" size="$3" onCheckedChange={() => setEnabledElements(!isEnabled)}>
+              <Switch.Thumb animation="bouncy" />
+            </Switch>
+          </XStack>
+          <XStack flex={1}>
+            <Button onPress={pickImg} backgroundColor={'cyan'} mx={'$4'}><Text color={'$black2'}>Upload Image</Text></Button>
+            {
+            file ? (             
+                <Image
+                  source={{
+                    uri: file
+                  }}
+                  width="100%" height="100%"                
+                />
             ) : null
           }
+          </XStack>
+          {
+            !isEnabled ? (
+              <YStack>
+                <TextArea
+                  placeholder={'Description...'}
+                  multiline={true}
+                  style={{
+                    height: 130,
+                    borderRadius: 5,
+                    paddingHorizontal: 10,
+                    textAlignVertical: 'top',
+                  }}
+                />
+                <TextArea
+                  placeholder={'List Steps...'}
+                  multiline={true}
+                  style={{
+                    height: 130,
+                    borderRadius: 5,
+                    paddingHorizontal: 10,
+                    textAlignVertical: 'top',
+                  }}
+                />
+                <TextArea
+                  placeholder={'List Ingredients...'}
+                  multiline={true}
+                  style={{
+                    height: 130,
+                    borderRadius: 5,
+                    paddingHorizontal: 10,
+                    textAlignVertical: 'top',
+                  }}
+                />
+              </YStack>
+            ) : null
+          }
+          {
+            isEnabled ? (
+              <YStack>
+                <TextArea
+                  placeholder={'Caption...'}
+                  multiline={true}
+                  style={{
+                    height: 130,
+                    borderRadius: 5,
+                    paddingHorizontal: 10,
+                    textAlignVertical: 'top',
+                  }}
+                />
+              </YStack>) : null
+          }
+          <Button backgroundColor={'cyan'} mx={'$4'}>
+            <Text color={'$black2'}>Post</Text>
+          </Button>
         </YStack>
-        {
-          !isEnabled ? (
-            <YStack>
-              <TextInput
-                placeholder={'Description...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }}
-              />
-              <TextInput
-                placeholder={'List Steps...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }}
-              />
-              <TextInput
-                placeholder={'List Ingredients...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }}
-              />
-            </YStack>
-          ) : null
-        }
-        {
-          isEnabled ? (
-            <YStack>
-              <TextInput
-                placeholder={'Upload IMG...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }} />
-              <TextInput
-                placeholder={'Caption...'}
-                multiline={true}
-                style={{
-                  height: 130,
-                  borderRadius: 5,
-                  paddingHorizontal: 10,
-                  textAlignVertical: 'top',
-                }}
-              />
-            </YStack>) : null
-        }
-        <Button backgroundColor={'cyan'} mx={'$4'}>
-          <Text color={'$black2'}>Post</Text>
-        </Button>
-      </YStack>
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }

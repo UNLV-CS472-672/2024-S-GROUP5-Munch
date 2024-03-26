ac5a790371f50c6752655ca471f08a968352c8be
razonm <razonm@users.noreply.github.com>
2024-03-26 07:33:59 +0000

    Apply formatting changes

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -9,33 +9,37 @@
 import { Link } from 'expo-router';
 import { Dimensions } from 'react-native';
 import { View, ScrollView } from 'tamagui';
-import Post from '../../components/Post'
+import Post from '../../components/Post';
 export default function Index() {
-    return (
+  return (
     <ScrollView
-        snapToInterval={Dimensions.get("window").height-100}
-        disableIntervalMomentum={true}
-        decelerationRate="fast"
+      snapToInterval={Dimensions.get('window').height - 100}
+      disableIntervalMomentum={true}
+      decelerationRate='fast'
     >
-        {/*Example posts*/}
-        <Post
-            title="Greek salad"
-            description="Try my family's famous greek salad. It is sure to be a crowd pleaser with tangy and refreshing vegetables"
-            image={{uri:'https://picsum.photos/1080/1920'}}
-            avatarImage='https://picsum.photos/300/300'
-        />
-        <Post
-            title="Banana cream pie"
-            description="My auntie's banana cream pie is to die for! She uses reduced plantains as a secret ingredient"
-            image={{uri:'https://fastly.picsum.photos/id/376/1080/1920.jpg?hmac=jowyHgQNXzKXZpoM25AeaYGH1WewzmOT18IQO6A-KRY'}}
-            avatarImage='https://fastly.picsum.photos/id/788/300/300.jpg?hmac=vZbkqAx5e6Wjik2rP-gC-xLBHoE6tjXGGKebHc_0CAI'
-        />
-        <Post
-            title="Kimchi Fried Rice"
-            description="A quick an easy breakfast that I learned from a college roommate. You just need rice, kimchi, an egg, and whatever seasonings you want"
-            image={{uri:'https://fastly.picsum.photos/id/348/1080/1920.jpg?hmac=JaRpzUiCbui3ranCaln8sSuScy1nnnSVkh9FZVVRGVs'}}
-            avatarImage='https://fastly.picsum.photos/id/175/300/300.jpg?hmac=vC3FhlD3cqzNijD5Bt9mBvZCzA6MSj3QsQNrIBv1uSo'
-        />
+      {/*Example posts*/}
+      <Post
+        title='Greek salad'
+        description="Try my family's famous greek salad. It is sure to be a crowd pleaser with tangy and refreshing vegetables"
+        image={{ uri: 'https://picsum.photos/1080/1920' }}
+        avatarImage='https://picsum.photos/300/300'
+      />
+      <Post
+        title='Banana cream pie'
+        description="My auntie's banana cream pie is to die for! She uses reduced plantains as a secret ingredient"
+        image={{
+          uri: 'https://fastly.picsum.photos/id/376/1080/1920.jpg?hmac=jowyHgQNXzKXZpoM25AeaYGH1WewzmOT18IQO6A-KRY',
+        }}
+        avatarImage='https://fastly.picsum.photos/id/788/300/300.jpg?hmac=vZbkqAx5e6Wjik2rP-gC-xLBHoE6tjXGGKebHc_0CAI'
+      />
+      <Post
+        title='Kimchi Fried Rice'
+        description='A quick an easy breakfast that I learned from a college roommate. You just need rice, kimchi, an egg, and whatever seasonings you want'
+        image={{
+          uri: 'https://fastly.picsum.photos/id/348/1080/1920.jpg?hmac=JaRpzUiCbui3ranCaln8sSuScy1nnnSVkh9FZVVRGVs',
+        }}
+        avatarImage='https://fastly.picsum.photos/id/175/300/300.jpg?hmac=vC3FhlD3cqzNijD5Bt9mBvZCzA6MSj3QsQNrIBv1uSo'
+      />
     </ScrollView>
-    );
+  );
 }
--- frontend/components/Post.tsx
+++ frontend/components/Post.tsx
@@ -1,97 +1,98 @@
 /*  Post Component
-*    Custom component to create posts. Currently strictly UI, so the
-*    post's variables (eg likes, comments) are not passed yet.
-*       Parameters:
-*           title
-*              title of the recipe; default is "A yummy recipe"
-*          description
-*             description of the recipe; default is "Munch on this
-*              delicious recipe!"
-*           image
-*               the background image; used to fulfill the source prop
-*               from Tamagui's Image.
-*           avatarImage -
-*              the avatar image; used to fulfill the src prop for
-*               Avatar.Image from Tamagui's Avatar
-*/
+ *    Custom component to create posts. Currently strictly UI, so the
+ *    post's variables (eg likes, comments) are not passed yet.
+ *       Parameters:
+ *           title
+ *              title of the recipe; default is "A yummy recipe"
+ *          description
+ *             description of the recipe; default is "Munch on this
+ *              delicious recipe!"
+ *           image
+ *               the background image; used to fulfill the source prop
+ *               from Tamagui's Image.
+ *           avatarImage -
+ *              the avatar image; used to fulfill the src prop for
+ *               Avatar.Image from Tamagui's Avatar
+ */
 
 import { TouchableHighlight, Dimensions } from 'react-native';
 import { Button, YStack, Image, Avatar, Text, View } from 'tamagui';
-import { Title, Subtitle } from '../tamagui.config.ts'
+import { Title, Subtitle } from '../tamagui.config.ts';
 
-export default function Post(
-    {
-        title="A yummy recipe",
-        description="Munch on this delicious recipe!",
-        image, avatarImage
-    }
-){
-    const width = Dimensions.get('window').width;
-    const height = Dimensions.get('window').height - 100;
-    return(
-        <View width={width}
-              height={height}
-              flexDirection = 'row-reverse'
+export default function Post({
+  title = 'A yummy recipe',
+  description = 'Munch on this delicious recipe!',
+  image,
+  avatarImage,
+}) {
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height - 100;
+  return (
+    <View width={width} height={height} flexDirection='row-reverse'>
+      {/*Background Image*/}
+      <Image
+        source={image}
+        width={width}
+        height={height}
+        resizeImage={'contain'}
+        position={'absolute'}
+      />
+      {/*Right-hand functions: Avatar, like, comment, share*/}
+      <YStack
+        backgroundColor={'grey'}
+        width={70}
+        height={320}
+        y={300}
+        justifyContent={'space-around'}
+        alignItems={'center'}
+      >
+        {/*Avatar*/}
+        <Avatar circular size='$6'>
+          <Avatar.Image src={avatarImage} />
+          <Avatar.Fallback bc='pink' />
+        </Avatar>
+        {/*Like*/}
+        <TouchableHighlight
+          onPress={() => console.log('Button pressed')}
+          underlayColor='crimson'
+          activeOpacity={1}
         >
-            {/*Background Image*/}
-            <Image  source={image}
-                    width={width}
-                    height={height}
-                    resizeImage={'contain'}
-                    position={'absolute'}
-            />
-            {/*Right-hand functions: Avatar, like, comment, share*/}
-            <YStack backgroundColor={'grey'}
-                    width={70}
-                    height={320}
-                    y={300}
-                    justifyContent={'space-around'}
-                    alignItems={'center'}
-            >
-                {/*Avatar*/}
-                <Avatar circular size='$6'>
-                    <Avatar.Image src={avatarImage}/>
-                    <Avatar.Fallback bc ="pink"/>
-                </Avatar>
-                {/*Like*/}
-                <TouchableHighlight onPress={() => console.log('Button pressed')}
-                                    underlayColor='crimson'
-                                    activeOpacity={1}
-                >
-                    <Image source={require('../assets/images/heart.png')} />
-                </TouchableHighlight>
-                {/*Comment*/}
-                <Button size='$5' circular>comm</Button>
-                {/*Bookmark*/}
-                <Button size='$5' circular>save</Button>
-                {/*Share*/}
-                <Button size='$5' circular>share</Button>
-            </YStack>
-            {/*Title, description, recipe*/}
-            <YStack backgroundColor={'grey'}
-                    width={300}
-                    height={200}
-                    justifyContent={'flex-end'}
-                    alignSelf={'flex-end'}
-                    x={-20}
-                    y={-20}
-            >
-                <YStack>
-                    {/*Recipe title*/}
-                    <Subtitle>{title}</Subtitle>
-                    {/*Recipe description*/}
-                    <Text>{description}</Text>
-                </YStack>
-                {/*The recipe*/}
-                <Button size='$5'
-                        borderRadius={25}
-                        width={250}
-                        radius={0}
-                        padding={10}
-
-                >The Recipe</Button>
-            </YStack>
-        </View>
-
-    )
+          <Image source={require('../assets/images/heart.png')} />
+        </TouchableHighlight>
+        {/*Comment*/}
+        <Button size='$5' circular>
+          comm
+        </Button>
+        {/*Bookmark*/}
+        <Button size='$5' circular>
+          save
+        </Button>
+        {/*Share*/}
+        <Button size='$5' circular>
+          share
+        </Button>
+      </YStack>
+      {/*Title, description, recipe*/}
+      <YStack
+        backgroundColor={'grey'}
+        width={300}
+        height={200}
+        justifyContent={'flex-end'}
+        alignSelf={'flex-end'}
+        x={-20}
+        y={-20}
+      >
+        <YStack>
+          {/*Recipe title*/}
+          <Subtitle>{title}</Subtitle>
+          {/*Recipe description*/}
+          <Text>{description}</Text>
+        </YStack>
+        {/*The recipe*/}
+        <Button size='$5' borderRadius={25} width={250} radius={0} padding={10}>
+          The Recipe
+        </Button>
+      </YStack>
+    </View>
+  );
 }

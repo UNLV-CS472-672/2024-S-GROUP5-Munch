f14b8ff192df7da32e024dfa844ee76dc8f4ba3c
honeepuffs <honeepuffs@users.noreply.github.com>
2024-03-26 23:56:53 +0000

    Apply formatting changes

--- frontend/components/recipe.tsx
+++ frontend/components/recipe.tsx
@@ -8,34 +8,46 @@ import { Container, Main, Subtitle, Title } from '../../tamagui.config.ts';
 export default function Recipe() {
   const { name } = useLocalSearchParams();
   const router = useRouter();
-  {/**/}
+  {
+    /**/
+  }
   const BackButton = () => (
     <Button
       unstyled
-      flexDirection="row"
-      backgroundColor="transparent"
+      flexDirection='row'
+      backgroundColor='transparent'
       paddingLeft={0}
       pressStyle={{ opacity: 0.5 }}
       onPress={router.back}
-      icon={<Feather name="chevron-left" size={16} color="#007AFF" />}>
-      <Text color="#007AFF">Back</Text>
+      icon={<Feather name='chevron-left' size={16} color='#007AFF' />}
+    >
+      <Text color='#007AFF'>Back</Text>
     </Button>
   );
 
   return (
     <Container>
-      <Stack.Screen options={{ title: 'Super Cool Recipe', headerCenter: () => <BackButton /> }} />
+      <Stack.Screen
+        options={{
+          title: 'Super Cool Recipe',
+          headerCenter: () => <BackButton />,
+        }}
+      />
       <Main>
         <YStack>
-          <BackButton/>
+          <BackButton />
           <Image
-            style={{width: 400, height: 300}}
-            resizeMode="contain"
-            source={{ uri: 'https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad.jpg'}}
+            style={{ width: 400, height: 300 }}
+            resizeMode='contain'
+            source={{
+              uri: 'https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad.jpg',
+            }}
           />
-          <Title color="white">Recipe Title</Title>
-          <Subtitle color="white">Showing recipe details for user {name}.</Subtitle>
-          <Text color="white">steps, testing, testing</Text>
+          <Title color='white'>Recipe Title</Title>
+          <Subtitle color='white'>
+            Showing recipe details for user {name}.
+          </Subtitle>
+          <Text color='white'>steps, testing, testing</Text>
         </YStack>
       </Main>
     </Container>

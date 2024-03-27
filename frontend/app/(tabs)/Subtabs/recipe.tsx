7680bc78ba50af429e7e4e74ca6b4e36cad3f46c
honeepuffs <honeepuffs@users.noreply.github.com>
2024-03-27 04:29:21 +0000

    Apply formatting changes

--- frontend/app/(tabs)/Subtabs/recipe.tsx
+++ frontend/app/(tabs)/Subtabs/recipe.tsx
@@ -8,40 +8,52 @@ import { Container, Main, Subtitle, Title } from '../../../tamagui.config.ts';
 export default function Recipe() {
   const { name } = useLocalSearchParams();
   const router = useRouter();
-  {/*This is the back button that goes back to the home page*/}
+  {
+    /*This is the back button that goes back to the home page*/
+  }
   const BackButton = () => (
     <Button
       unstyled
-      flexDirection="row"
-      backgroundColor="transparent"
+      flexDirection='row'
+      backgroundColor='transparent'
       paddingLeft={0}
       pressStyle={{ opacity: 0.5 }}
       onPress={router.back}
-      icon={<Feather name="chevron-left" size={16} color="#007AFF" />}>
-      <Text color="#007AFF">Back</Text>
+      icon={<Feather name='chevron-left' size={16} color='#007AFF' />}
+    >
+      <Text color='#007AFF'>Back</Text>
     </Button>
   );
 
-  return(
+  return (
     <Container>
-      <Stack.Screen options={{ title: 'Super Cool Recipe', headerCenter: () => <BackButton /> }} />
+      <Stack.Screen
+        options={{
+          title: 'Super Cool Recipe',
+          headerCenter: () => <BackButton />,
+        }}
+      />
       <Main>
-          {/* This is the main body of the recipe page*/}
+        {/* This is the main body of the recipe page*/}
         <YStack>
           {/* This Button navigates back to home*/}
-          <BackButton/>
+          <BackButton />
           {/* This Image is a Placeholder but shows the food*/}
           <Image
-            style={{width: 400, height: 300}}
-            resizeMode="contain"
-            source={{ uri: 'https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad.jpg'}}
+            style={{ width: 400, height: 300 }}
+            resizeMode='contain'
+            source={{
+              uri: 'https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad.jpg',
+            }}
           />
           {/* Title of the Recipe*/}
-          <Title color="white">Recipe Title</Title>
+          <Title color='white'>Recipe Title</Title>
           {/* Small bio with author*/}
-          <Subtitle color="white">Showing recipe details for user {name}.</Subtitle>
+          <Subtitle color='white'>
+            Showing recipe details for user {name}.
+          </Subtitle>
           {/* Steps everything else etc*/}
-          <Text color="white">steps, testing, testing</Text>
+          <Text color='white'>steps, testing, testing</Text>
         </YStack>
       </Main>
     </Container>

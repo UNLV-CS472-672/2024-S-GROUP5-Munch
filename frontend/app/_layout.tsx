9b0b6248e72baa1b04ad4f1a30b623b76c7b230b
Stacey Lai <staceyclai@gmail.com>
2024-03-26 19:04:05 -0700

    Leftover merge conflict error

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -55,7 +55,6 @@ export default function RootLayout() {
       <QueryClientProvider client={queryClient}>
           <RootLayoutNav />
       </QueryClientProvider>
-      <RootLayoutNav />
     </ClerkProvider>
   );
 }

0c22a4340a4b742206292cf80b5914c4c638305f
Stacey Lai <staceyclai@gmail.com>
2024-03-26 23:15:36 -0700

    Create Edit Recipe Page

---
+++ frontend/app/edit/editRecipe.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  H1,
+  H2,
+  H4,
+  YStack,
+  Image,
+  Separator,
+  SizableText,
+  Tabs,
+  XStack,
+  H5,
+  Text,
+  TextArea,
+  View,
+} from 'tamagui';
+import type { TabsContentProps } from 'tamagui';
+
+export default function Create() {
+  const [isEnabled, setEnabledElements] = useState(false);
+
+  return (
+    <View>
+          <YStack>
+            <H4 style={{ alignSelf: 'center', paddingTop: 75 }}>EDIT RECIPE</H4>
+                <TextArea
+                  placeholder={'Change your Description...'}
+                  multiline={true}
+                  style={{
+                    height: 130,
+                    borderRadius: 5,
+                    paddingHorizontal: 10,
+                    textAlignVertical: 'top',
+                  }}
+                />
+                <TextArea
+                  placeholder={'Change your Steps...'}
+                  multiline={true}
+                  style={{
+                    height: 130,
+                    borderRadius: 5,
+                    paddingHorizontal: 10,
+                    textAlignVertical: 'top',
+                  }}
+                />
+                <TextArea
+                  placeholder={'Change your Ingredients...'}
+                  multiline={true}
+                  style={{
+                    height: 130,
+                    borderRadius: 5,
+                    paddingHorizontal: 10,
+                    textAlignVertical: 'top',
+                  }}
+                />
+          </YStack>
+        </View>
+  );
+}

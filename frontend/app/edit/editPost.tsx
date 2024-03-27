ccb8bdd4878a3adb10065a3c8685f8665d93f09c
StacL <StacL@users.noreply.github.com>
2024-03-27 05:48:56 +0000

    Apply formatting changes

--- frontend/app/edit/editPost.tsx
+++ frontend/app/edit/editPost.tsx
@@ -18,22 +18,21 @@ import {
 import type { TabsContentProps } from 'tamagui';
 
 export default function EditPost() {
-
   return (
-  <SafeAreaView>
-    <View>
-      <H4 style={{ alignSelf: 'center',  paddingTop: 10 }}>EDIT POST</H4>
-      <TextArea
-        placeholder={'Change your Caption...'}
-        multiline={true}
-        style={{
-          height: 130,
-          borderRadius: 5,
-          paddingHorizontal: 10,
-          textAlignVertical: 'top',
-        }}
-      />
-    </View>
-  </SafeAreaView>
+    <SafeAreaView>
+      <View>
+        <H4 style={{ alignSelf: 'center', paddingTop: 10 }}>EDIT POST</H4>
+        <TextArea
+          placeholder={'Change your Caption...'}
+          multiline={true}
+          style={{
+            height: 130,
+            borderRadius: 5,
+            paddingHorizontal: 10,
+            textAlignVertical: 'top',
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }

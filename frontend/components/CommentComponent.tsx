16a2b6316dcdf44cfea774b2a5e445e1c7960700
honeepuffs <honeepuffs@users.noreply.github.com>
2024-03-27 05:13:10 +0000

    Apply formatting changes

--- frontend/components/CommentComponent.tsx
+++ frontend/components/CommentComponent.tsx
@@ -1,25 +1,18 @@
 import { Link } from 'expo-router';
 import { TouchableHighlight, Dimensions } from 'react-native';
 import { Button, YStack, Image, Avatar, Text, View } from 'tamagui';
-import { Title, Subtitle } from '../tamagui.config.ts'
+import { Title, Subtitle } from '../tamagui.config.ts';
 
-
-export default function CommentComponent(
-    {
-        id, // id,
-        parent_id, // parent comment id,
-        name, // commenting person name,
-        image, // commenting person image,
-        text // comment text,
-    }
-){
-    const width = Dimensions.get('window').width;
-    const height = Dimensions.get('window').height - 100;
-    return(
-        <View width={width}
-                      height={height}
-                      flexDirection = 'row-reverse'
-                    >
-                </View>
-    )
+export default function CommentComponent({
+  id, // id,
+  parent_id, // parent comment id,
+  name, // commenting person name,
+  image, // commenting person image,
+  text, // comment text,
+}) {
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height - 100;
+  return (
+    <View width={width} height={height} flexDirection='row-reverse'></View>
+  );
 }

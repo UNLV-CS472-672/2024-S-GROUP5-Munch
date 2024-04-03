73b9f030974c2f9d59e0bf39e7ac9f6758c62635
noodaj <jadon.loi.68@gmail.com>
2024-04-03 14:07:14 -0700

    moved util files outside of app directory

---
+++ frontend/utils/getCurrentDateTime.ts
@@ -0,0 +1,11 @@
+export function getCurrentDateTime(): string {
+  const date = new Date();
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+}
---
+++ frontend/utils/tokenCache.ts
@@ -0,0 +1,18 @@
+import { getItemAsync, setItemAsync } from 'expo-secure-store';
+export const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+
+  async saveToken(key: string, value: string) {
+    try {
+      return setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};

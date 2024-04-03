e9caba7a69bcc8767cc668a35ce9410bdefd7eed
noodaj <jadon.loi.68@gmail.com>
2024-04-03 15:06:53 -0700

    moved util files outside of app directory

--- frontend/app/utils/typeGuard.ts
+++
@@ -1,9 +0,0 @@
-import { Byte, Recipe } from '@/types/post';
-
-export const isByte = (post: Byte | Recipe): post is Byte => {
-  return 'location' in post;
-};
-
-export const isRecipe = (post: Byte | Recipe): post is Recipe => {
-  return 'steps' in post;
-};

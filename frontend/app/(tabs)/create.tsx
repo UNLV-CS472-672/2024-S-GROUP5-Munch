f2b896651c1a6fa21a3d0b48398cfb0dc3a59fa8
Michelle McGowan <mcgowm1@unlv.nevada.edu>
2024-04-06 14:29:40 -0700

    Schema + Error Check
    - follows logic of login/register
    - implements schema for input checking
    - changes design

--- frontend/app/(tabs)/create.tsx
+++ frontend/app/(tabs)/create.tsx
@@ -16,18 +16,63 @@ import {
   Text,
   TextArea,
   Switch,
+  Form
 } from 'tamagui';
+import { ByteSchema, ByteSchemaInputs, RecipeSchema, RecipeSchemaInputs } from '@/types/postInput';
+import { Controller, SubmitHandler, set, useForm } from 'react-hook-form';
+import UserInput from '@/components/UserInput';
 
 export default function Create() {
   const [isEnabled, setEnabledElements] = useState(false);
 
+  const {
+    handleSubmit: handleSubmitByte,
+    control: controlByte,
+    formState: { errors: errorsByte },
+  } = useForm<ByteSchemaInputs>({
+    resolver: zodResolver(ByteSchema),
+    defaultValues: {
+      description: ''
+    },
+  });
+
+  const {
+    handleSubmit: handleSubmitRecipe,
+    control: controlRecipe,
+    formState: { errors: errorsRecipe },
+  } = useForm<RecipeSchemaInputs>({
+    resolver: zodResolver(RecipeSchema),
+    defaultValues: {
+      descr: '',
+      ingredients: '',
+      steps: ''
+    },
+  });
+  const createByte: SubmitHandler<ByteSchemaInputs> = (data) => {
+    console.log("dfljkfd");
+    try {
+      console.log(data.description);
+    } catch (err) {
+      // error
+    }
+  };
+  const createRecipe: SubmitHandler<RecipeSchemaInputs> = (data) => {
+    try {
+      console.log(data.descr);
+      console.log(data.steps);
+      console.log(data.ingredients);
+    } catch (err) {
+      //error
+    }
+  };
+
   return (
     <View>
       <YStack>
-        <H4 style={{ alignSelf: 'center', color: '$accentColor' }}>NEW POST</H4>
+        <H4 style={{ alignSelf: 'center' }}>NEW POST</H4>
         <XStack>
-          <Text color={'$accentColor'} fontSize='$5' paddingStart='$5'>
-            is this a recipe?
+          <Text fontSize='$5' paddingStart='$5'>
+            Is this a recipe?
           </Text>
           <Switch
             marginLeft='$2'
@@ -37,43 +82,31 @@ export default function Create() {
             <Switch.Thumb animation='bouncy' />
           </Switch>
         </XStack>
-        {isEnabled ? (
-          <YStack>
-            <TextArea
-              placeholder={'Description...'}
-              multiline={true}
-              style={{
-                height: 130,
-                borderRadius: 5,
-                paddingHorizontal: 10,
-                textAlignVertical: 'top',
-              }}
-            />
-            <TextArea
-              placeholder={'List Steps...'}
-              multiline={true}
-              style={{
-                height: 130,
-                borderRadius: 5,
-                paddingHorizontal: 10,
-                textAlignVertical: 'top',
-              }}
-            />
-            <TextArea
-              placeholder={'List Ingredients...'}
-              multiline={true}
-              style={{
-                height: 130,
-                borderRadius: 5,
-                paddingHorizontal: 10,
-                textAlignVertical: 'top',
-              }}
-            />
-          </YStack>
-        ) : null}
-        {!isEnabled ? (
-          <YStack>
-            <TextArea
+        <Form
+          onSubmit={handleSubmitByte(createByte)}
+          gap={'$2'}
+          marginHorizontal={15}
+          px={'$2'}
+        >
+          {!isEnabled ? (
+            <YStack>
+              <Controller
+                name='description'
+                control={controlByte}
+                render={({ field }) => (
+                  <UserInput
+                    field={field}
+                    labelID={'description'}
+                    placeholder={'Whatcha munchin\' on?'}
+                    key={'description'}
+                    sx={{ borderWidth: 1, size: '$5', width: '100%' }}
+                  />
+                )}
+              />
+              {errorsByte.description?.message && (
+                <Text color={'$red10'}>{errorsByte.description.message}</Text>
+              )}
+              {/* <TextArea
               placeholder={'Upload IMG...'}
               multiline={true}
               style={{
@@ -82,23 +115,83 @@ export default function Create() {
                 paddingHorizontal: 10,
                 textAlignVertical: 'top',
               }}
-            />
-            <TextArea
-              placeholder={'Caption...'}
-              multiline={true}
-              style={{
-                height: 130,
-                borderRadius: 5,
-                paddingHorizontal: 10,
-                textAlignVertical: 'top',
-              }}
-            />
-          </YStack>
-        ) : null}
-        <Button backgroundColor={'cyan'} mx={'$4'} unstyled>
-          <Text>Post</Text>
-        </Button>
+            /> */}
+              <Form.Trigger asChild>
+                <Button backgroundColor={'$red9'}>
+                  Post
+                </Button>
+              </Form.Trigger>
+            </YStack>
+          ) : null}
+        </Form>
+        <Form
+          onSubmit={handleSubmitRecipe(createRecipe)}
+          gap={'$2'}
+          marginHorizontal={15}
+          px={'$2'}
+        >
+          {isEnabled ? (
+            <YStack>
+              <Controller
+                name='descr'
+                control={controlRecipe}
+                render={({ field }) => (
+                  <UserInput
+                    field={field}
+                    labelID={'descr'}
+                    placeholder={'Whatcha munchin\' on?'}
+                    key={'descr'}
+                    sx={{ borderWidth: 1, size: '$5', width: '100%' }}
+                  />
+                )}
+              />
+              {errorsRecipe.descr?.message && (
+                <Text color={'$red10'}>{errorsRecipe.descr.message}</Text>
+              )}
+              <Controller
+                name='ingredients'
+                control={controlRecipe}
+                render={({ field }) => (
+                  <UserInput
+                    field={field}
+                    labelID={'ingredients'}
+                    placeholder={'What\'s in your munch?'}
+                    key={'ingredients'}
+                    sx={{ borderWidth: 1, size: '$5', width: '100%' }}
+                  />
+                )}
+              />
+              {errorsRecipe.ingredients?.message && (
+                <Text color={'$red10'}>{errorsRecipe.ingredients.message}</Text>
+              )}
+              {/* {errorsRecipe.description?.message && (
+              <Text color={'$red10'}>{errors.description.message}</Text>
+            )} */}
+              <Controller
+                name='steps'
+                control={controlRecipe}
+                render={({ field }) => (
+                  <UserInput
+                    field={field}
+                    labelID={'steps'}
+                    placeholder={'How did you make your munch?'}
+                    key={'steps'}
+                    sx={{ borderWidth: 1, size: '$5', width: '100%' }}
+                  />
+                )}
+              />
+              {errorsRecipe.steps?.message && (
+                <Text color={'$red10'}>{errorsRecipe.steps.message}</Text>
+              )}
+              <Form.Trigger asChild>
+                <Button backgroundColor={'$red9'}>
+                  Post
+                </Button>
+              </Form.Trigger>
+            </YStack>
+          ) : null}
+        </Form>
       </YStack>
-    </View>
+    </View >
   );
 }
--- frontend/types/postInput.ts
+++ frontend/types/postInput.ts
@@ -10,61 +10,61 @@ const sizeInMB = (sizeInBytes: number, decimalsNum = 2) => {
 };
 
 export type ByteState = {
-  image?: string[];
+  //image?: string[];
   description?: string;
 };
 
 export type RecipeState = {
   image?: string[];
-  description?: string;
+  descr?: string;
   ingredients?: string;
   steps?: string;
 };
 
 // byte post
 export const ByteSchema = z.object({
-  image: z
-    .custom<FileList>()
-    .refine((files) => {
-      return Array.from(files ?? []).length !== 0;
-    }, 'Image is required')
-    .refine((files) => {
-      return Array.from(files ?? []).every(
-        (file) => sizeInMB(file.size) <= MAX_IMAGE_SIZE,
-      );
-    }, `The maximum image size is ${MAX_IMAGE_SIZE}MB`)
-    .refine((files) => {
-      return Array.from(files ?? []).every((file) =>
-        ACCEPTED_IMAGE_TYPES.includes(file.type),
-      );
-    }, 'File type is not supported'),
-  description: z.string().optional(),
+  // image: z
+  //   .custom<FileList>()
+  //   .refine((files) => {
+  //     return Array.from(files ?? []).length !== 0;
+  //   }, 'Image is required')
+  //   .refine((files) => {
+  //     return Array.from(files ?? []).every(
+  //       (file) => sizeInMB(file.size) <= MAX_IMAGE_SIZE,
+  //     );
+  //   }, `The maximum image size is ${MAX_IMAGE_SIZE}MB`)
+  //   .refine((files) => {
+  //     return Array.from(files ?? []).every((file) =>
+  //       ACCEPTED_IMAGE_TYPES.includes(file.type),
+  //     );
+  //   }, 'File type is not supported'),
+  description: z.string().min(1, { message: 'Description must have one character.' }),
 });
 
 export type ByteSchemaInputs = z.infer<typeof ByteSchema>;
 
 // recipe post
 export const RecipeSchema = z.object({
-  image: z
-    .custom<FileList>()
-    .refine((files) => {
-      return Array.from(files ?? []).length !== 0;
-    }, 'Image is required')
-    .refine((files) => {
-      return Array.from(files ?? []).every(
-        (file) => sizeInMB(file.size) <= MAX_IMAGE_SIZE,
-      );
-    }, `The maximum image size is ${MAX_IMAGE_SIZE}MB`)
-    .refine((files) => {
-      return Array.from(files ?? []).every((file) =>
-        ACCEPTED_IMAGE_TYPES.includes(file.type),
-      );
-    }, 'File type is not supported'),
-  description: z.string().optional(),
+  // image: z
+  //   .custom<FileList>()
+  //   .refine((files) => {
+  //     return Array.from(files ?? []).length !== 0;
+  //   }, 'Image is required')
+  //   .refine((files) => {
+  //     return Array.from(files ?? []).every(
+  //       (file) => sizeInMB(file.size) <= MAX_IMAGE_SIZE,
+  //     );
+  //   }, `The maximum image size is ${MAX_IMAGE_SIZE}MB`)
+  //   .refine((files) => {
+  //     return Array.from(files ?? []).every((file) =>
+  //       ACCEPTED_IMAGE_TYPES.includes(file.type),
+  //     );
+  //   }, 'File type is not supported'),
+  descr: z.string().min(3, { message: 'Description must have one character.' }),
   ingredients: z
     .string()
     .min(3, { message: 'Recipe must have at least one ingredient.' }),
   steps: z.string().min(3, { message: 'Recipe must have at least one step.' }),
 });
 
-export type RecipeSchemaInputs = z.infer<typeof ByteSchema>;
+export type RecipeSchemaInputs = z.infer<typeof RecipeSchema>;

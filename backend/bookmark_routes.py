287da0d004119e4045384cb2adbc8cd26387c2da
tadakane <tadakane@users.noreply.github.com>
2024-04-07 00:19:38 +0000

    Apply formatting changes

--- backend/bookmark_routes.py
+++ backend/bookmark_routes.py
@@ -6,6 +6,7 @@ from helper_functions import try_connect_to_db
 
 bookmark_bp = Blueprint("bookmark", __name__)
 
+
 # Toggle bookmark on or off
 @bookmark_bp.route("/api/users/<user_id>/bookmarks", methods=["PATCH"])
 def toggle_bookmark(user_id):
@@ -20,8 +21,10 @@ def toggle_bookmark(user_id):
 
     # make sure post is a field in that data
     if "post" not in res:
-        return (jsonify({"error": f"Missing post to bookmark"}),
-        status.HTTP_400_BAD_REQUEST)
+        return (
+            jsonify({"error": f"Missing post to bookmark"}),
+            status.HTTP_400_BAD_REQUEST,
+        )
 
     # copy data
     data = copy.deepcopy(res)
@@ -41,19 +44,30 @@ def toggle_bookmark(user_id):
         recipe_ids = [recipe.id for recipe in recipes_collection.stream()]
 
         # get post id from data, make sure it's an actual post or recipe, if not return error
-        if str(data["post"].split("/")[1]) not in post_ids and str(data["post"].split("/")[1]) not in recipe_ids:
-            return (jsonify({"error": f"Post or recipe requested to bookmark does not exist"}), 
-                    status.HTTP_400_BAD_REQUEST)
+        if (
+            str(data["post"].split("/")[1]) not in post_ids
+            and str(data["post"].split("/")[1]) not in recipe_ids
+        ):
+            return (
+                jsonify(
+                    {
+                        "error": f"Post or recipe requested to bookmark does not exist"
+                    }
+                ),
+                status.HTTP_400_BAD_REQUEST,
+            )
 
         # get user collection based on user id and convert to dict
         user_ref = db.collection("users").document(user_id)
         user_data = user_ref.get().to_dict()
 
         # convert the post from the data to a reference
-        post_ref =  db.document(data["post"])
+        post_ref = db.document(data["post"])
 
         # if bookmark is not in user database, add the reference to user database
-        if str(data["post"]) not in [bookmark.path for bookmark in user_data["bookmarks"]]:
+        if str(data["post"]) not in [
+            bookmark.path for bookmark in user_data["bookmarks"]
+        ]:
             user_data["bookmarks"].append(post_ref)
             user_ref.update(user_data)
         # if bookmark already in user database, remove the reference from user database
@@ -69,6 +83,5 @@ def toggle_bookmark(user_id):
         print("Error adding new bookmark:", str(e))
         return (
             jsonify({"error": "Error adding new bookmark"}),
-            status.HTTP_500_INTERNAL_SERVER_ERROR
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
-    

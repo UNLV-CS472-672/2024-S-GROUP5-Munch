4061af9e097e0f963e0dcbde6feb1bc2b27d67c2
tadakane <rortheford@gmail.com>
2024-04-08 15:03:18 -0700

    Add documentation to changes in get_our_own_businesses

--- backend/yelpAPI/fakeDataAPI.py
+++ backend/yelpAPI/fakeDataAPI.py
@@ -41,30 +41,38 @@ def get_business():
 # route gets 50 businessses from yelp API
 @yelp_bp.route("/api/<latitude>/<longitude>", methods=["GET"])
 def get_our_own_businesses(latitude, longitude):
-    # Grab a random state from states.py
+    # Get a random integer for the offset
     random_int = str(random.randint(0, 500))
-    # Use query filter to get 50 businesses from the random state provided, can change limit query to X amount
+    # Use query filter to get 50 businesses from provided latitude and longitude, can change limit query to X amount
+    # offset is from integer above, to get more random options
     response = requests.get(
         URL + "businesses/search?latitude=" + latitude + "&longitude=" + longitude + "&limit=50" + "&offset=" + random_int,
         headers=headers,
     )
 
+    # if successful response
     if response.status_code == 200:
+        # keep these fields from the yelp API
         kept_fields = ["coordinates", "location", "name", "price", "rating", "image_url", "description"]
 
         filtered_responses = []
+        # for each business "r"
         for r in response.json()["businesses"]:
+            # convert coordinates to one string
             latitude = r["coordinates"].get("latitude")
             longitude = r["coordinates"].get("longitude")
             if latitude is not None and longitude is not None:
                 r["coordinates"] = f"{latitude}, {longitude}"
             
+            # put all the categories into a "description"
             if "categories" in r:
                 r["description"] = ", ".join(category["title"] for category in r["categories"])
                     
+            # filter response to only keep the fields we want
             filtered_response = {key: value for key, value in r.items() if key in kept_fields}
             filtered_responses.append(filtered_response)
 
+        # do some formatting
         filtered_response_json = json.dumps(filtered_responses, indent=4)
         # Return the data from the API as JSON
         return filtered_response_json, 200

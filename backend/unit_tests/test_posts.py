4fd6318d56be1c8caa8d299ed5cb59a39ff851ac
FabrizV <fabriziovaldivia2001@gmail.com>
2024-04-07 17:59:29 -0700

    Added test cases to improve coverage

--- backend/unit_tests/test_posts.py
+++ backend/unit_tests/test_posts.py
@@ -86,10 +86,46 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(updated_post_mock_data, update_response.json)
         self.assertEqual(update_response.status_code, status.HTTP_200_OK)
 
+        # Test getting a non-existing post
+        bad_get_response = self.client.get(f"/api/posts/bad_id")
+        self.assertEqual(bad_get_response.status_code, status.HTTP_404_NOT_FOUND)
+
         # Get post
         get_response = self.client.get(f"/api/posts/{post_id}")
         self.assertEqual(updated_post_mock_data, get_response.json)
         self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+
+
+        # Data that will Error (mising a field)
+        bad_data = {
+            "author": f"users/{testing_user_id}",
+            "comments": [
+                {
+                    "author": f"users/{testing_user_id}",
+                    "comment": "Updating comment on my first post!",
+                    "comment_id": "comment_id",
+                    "creation_date": "2024-03-20 22:53:36.118000+00:00",
+                    "username": "test_user",
+                }
+            ],
+            "creation_date": "2024-03-20 21:41:30.786000+00:00",
+            "description": "This is my first post and I just edited it!",
+            "likes": 0,
+            "pictures": [],
+            "username": "test_user",
+        }
+
+        # Creating an invalid post
+        try:
+            response = self.client.post("/api/posts", json=bad_data)
+        except:
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        # Updating a post invalidly
+        try:
+            response = self.client.patch(f"/api/posts/invalid_data", json=bad_data)
+        except:
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         # Delete Post
         delete_response = self.client.delete(f"api/posts/{post_id}")

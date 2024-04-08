5043f1203772d49b3e8dbbc056c3008f240402d9
tadakane <tadakane@users.noreply.github.com>
2024-04-08 22:08:19 +0000

    Apply formatting changes

--- backend/yelpAPI/fakeDataAPI.py
+++ backend/yelpAPI/fakeDataAPI.py
@@ -46,14 +46,29 @@ def get_our_own_businesses(latitude, longitude):
     # Use query filter to get 50 businesses from provided latitude and longitude, can change limit query to X amount
     # offset is from integer above, to get more random options
     response = requests.get(
-        URL + "businesses/search?latitude=" + latitude + "&longitude=" + longitude + "&limit=50" + "&offset=" + random_int,
+        URL
+        + "businesses/search?latitude="
+        + latitude
+        + "&longitude="
+        + longitude
+        + "&limit=50"
+        + "&offset="
+        + random_int,
         headers=headers,
     )
 
     # if successful response
     if response.status_code == 200:
         # keep these fields from the yelp API
-        kept_fields = ["coordinates", "location", "name", "price", "rating", "image_url", "description"]
+        kept_fields = [
+            "coordinates",
+            "location",
+            "name",
+            "price",
+            "rating",
+            "image_url",
+            "description",
+        ]
 
         filtered_responses = []
         # for each business "r"
@@ -63,13 +78,17 @@ def get_our_own_businesses(latitude, longitude):
             longitude = r["coordinates"].get("longitude")
             if latitude is not None and longitude is not None:
                 r["coordinates"] = f"{latitude}, {longitude}"
-            
+
             # put all the categories into a "description"
             if "categories" in r:
-                r["description"] = ", ".join(category["title"] for category in r["categories"])
-                    
+                r["description"] = ", ".join(
+                    category["title"] for category in r["categories"]
+                )
+
             # filter response to only keep the fields we want
-            filtered_response = {key: value for key, value in r.items() if key in kept_fields}
+            filtered_response = {
+                key: value for key, value in r.items() if key in kept_fields
+            }
             filtered_responses.append(filtered_response)
 
         # do some formatting

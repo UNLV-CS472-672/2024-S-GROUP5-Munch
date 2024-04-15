5434d59fdb2c432fc52a0cd4f4896074d66cdfea
haiimkeith <haiimkeith@users.noreply.github.com>
2024-04-15 20:08:01 +0000

    Apply formatting changes

--- backend/run_tests.py
+++ backend/run_tests.py
@@ -13,7 +13,7 @@ def run_tests():
             "like_routes",
             "bookmark_routes",
             "comment_routes",
-            "follow_routes"
+            "follow_routes",
         ]
     )
     cov.start()

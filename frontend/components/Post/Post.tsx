dd58d6c25652dd755dd9ac7fa696fe48440db3eb
Michael Razon <ocmichaelrazon@gmail.com>
2024-04-20 16:07:07 -0700

    Add Like Button Functionality
    Added Like Button functionality with an example like button in Feed tab

--- frontend/components/Post/Post.tsx
+++ frontend/components/Post/Post.tsx
@@ -3,11 +3,15 @@ import { Byte, Recipe } from '@/types/post';
 import { getDateDifference } from '@/utils/getCurrentDateTime';
 import { isByte, isRecipe } from '@/utils/typeGuard';
 import { Link, useRouter } from 'expo-router';
-import React, { FC } from 'react';
+import React, { FC, useContext, useState } from 'react';
 import { Dimensions, Linking, Platform, SafeAreaView } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 import { Image, Text, XStack, YStack } from 'tamagui';
 import ButtonIcon from './ButtonIcon';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { UserContext } from '@/contexts/UserContext';
+import { useAuth } from '@clerk/clerk-expo';
+import axios from 'axios';
 
 interface PostProps {
   post: Byte | Recipe;
@@ -37,7 +41,76 @@ const Post: FC<PostProps> = ({ post }) => {
     Linking.openURL(Platform.OS === 'ios' ? iosLink : androidLink);
   };
 
-  const handleLike = async () => {};
+  // Used to query and mutate
+  const queryClient = useQueryClient();
+  const { token } = useContext(UserContext)
+  const { getToken, userId } = useAuth();
+
+  // Like button state
+  const [liked, setLiked] = useState(false);
+
+  // Data passed to like/unlike API
+  const likeData = {
+    user_id: userId,
+    post_id: key
+  }
+
+  // Get number of likes
+  const getLikes = async () => {
+    const response = await axios.get(
+        `${process.env.EXPO_PUBLIC_IP_ADDR}/api/posts/${key}`,
+        {
+            headers: { Authorization: `Bearer ${token}` }
+        },
+    );
+    return response.data.likes;
+  }
+
+  const {data: likesCount, error: likesError, isLoading: likesLoading} = useQuery({
+      queryKey: ["likes"],
+      queryFn: getLikes,
+  });
+
+  // Like/Unlike a post
+  const {mutateAsync: changeLikes, isPending, data, error } = useMutation({
+    mutationFn: async () => {
+        // Determine whether to like or unlike the post
+        const likeAction = liked ? 'like' : 'unlike'
+
+        // Status message to show the API call
+        console.log(`${process.env.EXPO_PUBLIC_IP_ADDR}/api/users/${userId}/${likeAction}/${key}`)
+
+        // Do the API call
+        const response = await axios.patch(
+            `${process.env.EXPO_PUBLIC_IP_ADDR}/api/users/${userId}/${likeAction}/${key}`,
+            likeData,
+            {
+                headers: {Authorization: `Bearer: ${await getToken()}`},
+            },
+        );
+        return response.data
+    },
+    // Update the like count
+    onSuccess: () => {
+        queryClient.invalidateQueries({queryKey:["likes"]});
+    },
+    // Show error message in console
+    onError: () => {
+        console.log("error:", error.message)
+    },
+  });
+
+  // Handle user liking the post
+  const handleLike = async () => {
+    // Invert the like state
+    setLiked(!liked)
+
+    // Status message
+    liked ? console.log("Liking the post!") : console.log("Unliking the post!")
+
+    // Like or unlike the post based on liked state
+    await changeLikes()
+  }
   const handleBookmark = async () => {};
   const carouselConfig = {
     width: width,
@@ -65,8 +138,18 @@ const Post: FC<PostProps> = ({ post }) => {
       />
       <YStack display='flex' rowGap={'$1'} marginBottom={'$10'}>
         <XStack display='flex' justifyContent='center'>
-          {/*Like*/}
-          <ButtonIcon iconName={'heart'} onPress={handleLike} />
+          <XStack alignItems='center'>
+            {/*Like*/}
+            <ButtonIcon iconName={'heart'} onPress={handleLike}/>
+            {/*Display number of likes*/}
+            {likesLoading? (
+                <Text>Loading</Text>
+            ) : likesError? (
+                <Text>-1</Text>
+            ) : (
+                <Text>{likesCount}</Text>
+            )}
+          </XStack>
           {/*Comment*/}
           <ButtonIcon
             iconName='comment'

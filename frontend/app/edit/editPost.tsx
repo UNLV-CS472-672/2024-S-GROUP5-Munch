935f22f84ccbc2f4c07522b15830ae1ef26ce38b
StacL <StacL@users.noreply.github.com>
2024-04-22 15:38:53 +0000

    Apply formatting changes

--- frontend/app/edit/editPost.tsx
+++ frontend/app/edit/editPost.tsx
@@ -42,28 +42,28 @@ export function EditPost({ post }: FC<PostProps>) {
   const queryClient = useQueryClient();
   let updatedPostData = post;
   const postLocation = usePathname();
-  const postId = postLocation.split("/").pop();
+  const postId = postLocation.split('/').pop();
 
   const { mutate, error } = useMutation({
-      mutationFn: (postData) => {
-        const response = axios.patch(
-          `${process.env.EXPO_PUBLIC_IP_ADDR}/api/posts/${postId}`,
-          postData,
-          {
-            headers: { Authorization: `Bearer ${token}` },
-          },
-        );
-        return response.data;
-      },
-      // Update the post with the edit
-      onSuccess: () => {
-        queryClient.invalidateQueries({ queryKey: ['post'] });
-      },
-      // Show error message in console
-      onError: () => {
-        console.log('error:', error.message);
-      },
-    });
+    mutationFn: (postData) => {
+      const response = axios.patch(
+        `${process.env.EXPO_PUBLIC_IP_ADDR}/api/posts/${postId}`,
+        postData,
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        },
+      );
+      return response.data;
+    },
+    // Update the post with the edit
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['post'] });
+    },
+    // Show error message in console
+    onError: () => {
+      console.log('error:', error.message);
+    },
+  });
 
   const {
     handleSubmit,
@@ -138,19 +138,19 @@ export function EditPost({ post }: FC<PostProps>) {
                     labelID='Description'
                     placeholder={post.description ?? 'description'}
                   />
-              )}
-            />
-          <Separator />
+                )}
+              />
+              <Separator />
 
-          <XStack alignSelf='flex-end' gap='$4' margin='$4'>
-            <Dialog.Close displayWhenAdapted asChild>
-            <Form.Trigger asChild>
-             <Button theme='active' aria-label='Close' type='submit'>
-                Save changes
-              </Button>
-            </Form.Trigger>
-            </Dialog.Close>
-          </XStack>
+              <XStack alignSelf='flex-end' gap='$4' margin='$4'>
+                <Dialog.Close displayWhenAdapted asChild>
+                  <Form.Trigger asChild>
+                    <Button theme='active' aria-label='Close' type='submit'>
+                      Save changes
+                    </Button>
+                  </Form.Trigger>
+                </Dialog.Close>
+              </XStack>
 
               <Unspaced>
                 <Dialog.Close asChild>

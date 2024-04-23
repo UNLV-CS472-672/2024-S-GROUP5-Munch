05c4c703df93552ebe2f12cec1eb74ac01553f36
Ryan Parker <rparker52003@gmail.com>
2024-04-23 07:02:23 -0700

    Fixed code that was removed after merge conflict fix

--- frontend/app/(tabs)/feed.tsx
+++ frontend/app/(tabs)/feed.tsx
@@ -1,22 +1,74 @@
-import { useAuth } from '@clerk/clerk-expo';
-import { SafeAreaView } from 'react-native';
+import Post from '@/components/Post/Post';
+import { UserContext } from '@/contexts/UserContext';
+import { UserType } from '@/types/firebaseTypes';
+import { Byte, Recipe, Comment } from '@/types/post';
+import { useQueries } from '@tanstack/react-query';
+import axios from 'axios';
+import React, { useCallback, useContext } from 'react';
+import { FlatList, SafeAreaView } from 'react-native';
 import { Text, View, Button } from 'tamagui';
 import LikeButton from '@/components/Post/LikeButton';
 
-export default function TabTwoScreen() {
+const Feed = () => {
+  const { token, user_data } = useContext(UserContext);
+
+  const { isLoading, posts } = useQueries({
+    queries: [
+      {
+        queryKey: ['userPosts'],
+        queryFn: async () => {
+          try {
+            const data = await Promise.all(
+              user_data.posts.map((post) =>
+                (async () =>
+                  (
+                    await axios.get<Byte | Recipe>(
+                      `${process.env.EXPO_PUBLIC_IP_ADDR}/api/${post}`,
+                      { headers: { Authorization: `Bearer ${token}` } },
+                    )
+                  ).data)(),
+              ),
+            );
+            return data;
+          } catch (error) {
+            console.error('Error fetching posts:', error);
+            return [];
+          }
+        },
+      },
+    ],
+    combine: (data) => {
+      return {
+        isLoading: data.some((query) => query.isLoading),
+        posts: data.flatMap((query) => query.data || []),
+      };
+    },
+  });
+
+  console.log(posts);
+
+  const renderItem = useCallback(
+    ({ item, index }: { item: Byte | Recipe; index: number }) => {
+      return <Post post={item} key={index} />;
+    },
+    [],
+  );
+
   return (
     <SafeAreaView>
       {!isLoading && (
-        <FlatList
-          data={posts}
-          renderItem={renderItem}
-          showsVerticalScrollIndicator={false}
-          decelerationRate={'fast'}
-          initialNumToRender={5}
-        />
+        // <FlatList
+        //   data={posts}
+        //   renderItem={renderItem}
+        //   showsVerticalScrollIndicator={false}
+        //   decelerationRate={'fast'}
+        //   initialNumToRender={5}
+        // />
+        <View>
+          <Text>Feed view</Text>
+          <LikeButton />
+        </View>
       )}
     </SafeAreaView>
   );
-}
-
-export default Feed;
+};

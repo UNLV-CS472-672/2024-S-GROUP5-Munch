a91d2a1cdb71dbe8ef5a7b454805cb06f6e207a4
Michael Razon <ocmichaelrazon@gmail.com>
2024-04-23 00:02:48 -0700

    Delete LikeButton.tsx
    Deleted LikeButton.tsx since it was just for testing. Change also reflected in feed.tsx

--- frontend/app/(tabs)/feed.tsx
+++ frontend/app/(tabs)/feed.tsx
@@ -1,13 +1,11 @@
 import { SafeAreaView } from 'react-native';
 import { Text, View, Button } from 'tamagui';
-import LikeButton from '@/components/Post/LikeButton';
 
 export default function TabTwoScreen() {
   return (
     <SafeAreaView>
       <View>
         <Text>Feed view</Text>
-        <LikeButton/>
       </View>
     </SafeAreaView>
   );

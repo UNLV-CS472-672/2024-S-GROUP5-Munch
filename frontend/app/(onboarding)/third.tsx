54271185794213c8f3e178405e18e5178aa2a4f1
britney <136287986+honeepuffs@users.noreply.github.com>
2024-04-23 21:21:01 -0700

    third onboarding page

---
+++ frontend/app/(onboarding)/third.tsx
@@ -0,0 +1,40 @@
+import UserInput from '@/components/UserInput';
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
+import { Ionicons } from '@expo/vector-icons';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'expo-router';
+import React, { useCallback, useEffect } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { View } from 'react-native';
+import Toast from 'react-native-toast-message';
+import { Button, Form, Separator, Text, XStack, YStack, Image } from 'tamagui';
+
+const Third = () => {
+  const router = useRouter();
+
+
+  return (
+    <View>
+    <Image
+        style={{ width: 450, height: 800 }}
+        source={{
+          uri: 'https://i.ibb.co/B4mbqSp/Munch-Onboarding-03.png',
+        }}
+      />
+      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}>
+      </YStack>
+      <YStack gap={'$1'} rowGap={'$2'}>
+        <Button
+          mx={'$4'}
+          onPress={() => {
+          router.push('/login');
+        }}
+        >
+          Join Us
+        </Button>
+      </YStack>
+    </View>
+  );
+};
+
+export default Third;

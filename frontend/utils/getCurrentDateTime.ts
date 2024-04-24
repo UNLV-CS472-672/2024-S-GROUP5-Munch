43480e374039a98a0bbcd25914ca808b0d28f454
Stacey Lai <staceyclai@gmail.com>
2024-04-24 09:33:22 -0700

    Fix create post again, and fix date

--- frontend/utils/getCurrentDateTime.ts
+++ frontend/utils/getCurrentDateTime.ts
@@ -1,13 +1,8 @@
 export function getCurrentDateTime(): string {
   const date = new Date();
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-  const year = date.getFullYear();
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-
-  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+  const isoString = date.toISOString();
+  const formattedDate = isoString.replace('T', ' ').replace('Z', '+00:00');
+  return formattedDate;
 }
 
 export const getDateDifference = (creation_date: string) => {

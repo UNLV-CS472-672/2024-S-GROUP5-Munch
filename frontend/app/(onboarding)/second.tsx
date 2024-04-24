16e2a550bd4becd27099385ec6b331bb3880261c
honeepuffs <honeepuffs@users.noreply.github.com>
2024-04-24 04:20:49 +0000

    Apply formatting changes

--- frontend/app/(onboarding)/second.tsx
+++ frontend/app/(onboarding)/second.tsx
@@ -12,23 +12,21 @@ import { Button, Form, Separator, Text, XStack, YStack, Image } from 'tamagui';
 const Second = () => {
   const router = useRouter();
 
-
   return (
     <View>
-    <Image
+      <Image
         style={{ width: 450, height: 800 }}
         source={{
           uri: 'https://i.ibb.co/HDXwNR3/Munch-Onboarding-02.png',
         }}
       />
-      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}>
-      </YStack>
+      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}></YStack>
       <YStack gap={'$1'} rowGap={'$2'}>
         <Button
           mx={'$4'}
           onPress={() => {
-          router.push('/third');
-        }}
+            router.push('/third');
+          }}
         >
           Continue
         </Button>

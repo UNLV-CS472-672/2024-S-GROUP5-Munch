77a81fe05d849e40a30e7f78609e35be94bc7e65
Stacey Lai <staceyclai@gmail.com>
2024-04-23 23:35:48 -0700

    Cleaned up and only shows on user's own posts

--- frontend/components/Post/DeletePostDialog.tsx
+++ frontend/components/Post/DeletePostDialog.tsx
@@ -60,7 +60,7 @@ export default function DeletePostDialog(postId) {
     <>
       <Dialog modal>
         <Dialog.Trigger asChild>
-          <Button backgroundColor={'$red9'}>Delete</Button>
+          <Button backgroundColor={'$red9'} minWidth='$14'>Delete</Button>
         </Dialog.Trigger>
 
         <Adapt when='sm' platform='touch'>

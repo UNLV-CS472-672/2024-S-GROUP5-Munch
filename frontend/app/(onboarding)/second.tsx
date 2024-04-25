fca86959da88eeed7b67dab6d7e4594db43e140b
honeepuffs <honeepuffs@users.noreply.github.com>
2024-04-25 01:23:19 +0000

    Apply formatting changes

--- frontend/app/(onboarding)/second.tsx
+++ frontend/app/(onboarding)/second.tsx
@@ -10,38 +10,40 @@ import Toast from 'react-native-toast-message';
 import { Button, Form, Separator, Text, XStack, YStack, Image } from 'tamagui';
 import { TouchableHighlight, Dimensions } from 'react-native';
 
-
 const Second = () => {
-   const router = useRouter();
-   const width = Dimensions.get('window').width;
-   const height = Dimensions.get('window').height;
+  const router = useRouter();
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;
 
-   return (
-     <View flexDirection='column' flex={1}>
-
-     <Image
-         width={width}
-         height={height}
-         source={{
-           uri: 'https://i.ibb.co/HDXwNR3/Munch-Onboarding-02.png',
-         }}
-         resizeImage={'contain'}
-         position={'absolute'}
-       />
-       <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}>
-       </YStack>
-       <YStack gap={'$1'} rowGap={'$2'} paddingTop={675} flexDirection='column-reverse'>
-         <Button
-           mx={'$4'}
-           onPress={() => {
-           router.push('/third');
-         }}
-         >
-           Continue
-         </Button>
-       </YStack>
-     </View>
-   );
- };
+  return (
+    <View flexDirection='column' flex={1}>
+      <Image
+        width={width}
+        height={height}
+        source={{
+          uri: 'https://i.ibb.co/HDXwNR3/Munch-Onboarding-02.png',
+        }}
+        resizeImage={'contain'}
+        position={'absolute'}
+      />
+      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}></YStack>
+      <YStack
+        gap={'$1'}
+        rowGap={'$2'}
+        paddingTop={675}
+        flexDirection='column-reverse'
+      >
+        <Button
+          mx={'$4'}
+          onPress={() => {
+            router.push('/third');
+          }}
+        >
+          Continue
+        </Button>
+      </YStack>
+    </View>
+  );
+};
 
 export default Second;

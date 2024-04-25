12d684c5693072dd90614b7d8d79d20baba5184f
honeepuffs <honeepuffs@users.noreply.github.com>
2024-04-25 02:29:03 +0000

    Apply formatting changes

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -142,7 +142,7 @@ function RootLayoutNav() {
       />
     ),
   };
-  
+
   const { isLoading } = useQuery({
     queryKey: ['userData', user],
     queryFn: async () => {
@@ -168,7 +168,7 @@ function RootLayoutNav() {
       return res;
     },
   });
-/*
+  /*
   const { signOut } = useAuth();
   const { isLoading } = useQuery({
     queryKey: ['userData', user],

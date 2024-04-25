ec7d2c5608a361e46018a7eb85fa9be88dc0c2b5
honeepuffs <honeepuffs@users.noreply.github.com>
2024-04-25 01:21:52 +0000

    Apply formatting changes

--- frontend/app/(onboarding)/first.tsx
+++ frontend/app/(onboarding)/first.tsx
@@ -7,7 +7,6 @@ import Toast from 'react-native-toast-message';
 import { Button, Form, Separator, Text, XStack, YStack, Image } from 'tamagui';
 import { TouchableHighlight, Dimensions } from 'react-native';
 
-
 const First = () => {
   const router = useRouter();
   const width = Dimensions.get('window').width;
@@ -15,8 +14,7 @@ const First = () => {
 
   return (
     <View flexDirection='column' flex={1}>
-
-    <Image
+      <Image
         width={width}
         height={height}
         source={{
@@ -25,14 +23,18 @@ const First = () => {
         resizeImage={'contain'}
         position={'absolute'}
       />
-      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}>
-      </YStack>
-      <YStack gap={'$1'} rowGap={'$2'} paddingTop={675} flexDirection='column-reverse'>
+      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}></YStack>
+      <YStack
+        gap={'$1'}
+        rowGap={'$2'}
+        paddingTop={675}
+        flexDirection='column-reverse'
+      >
         <Button
           mx={'$4'}
           onPress={() => {
-          router.push('/second');
-        }}
+            router.push('/second');
+          }}
         >
           Continue
         </Button>

ddbd04b5f1083f43b66e3584714866b06851a27c
haiimkeith <haiimkeith@users.noreply.github.com>
2024-04-25 00:11:35 +0000

    Apply formatting changes

--- backend/unit_tests/test_yelp.py
+++ backend/unit_tests/test_yelp.py
@@ -9,26 +9,33 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import status
 from server import app
 
+
 class YelpTest(TestCase):
     # Set up client
     def setUp(self):
         self.app = app
         self.app.testing = True
         self.client = self.app.test_client()
-    
+
     def test_yelp(self):
         # Get Businesses test
         create_businesses = self.client.get(f"/api/business")
         self.assertEqual(create_businesses.status_code, status.HTTP_200_OK)
-        
+
         # Create valid coordinates
         valid_longitude = -122.083922
         valid_latitude = 37.4220936
-        
+
         # Get local businesses
-        get_local_businesses = self.client.get(f"/api/{valid_latitude}/{valid_longitude}")
+        get_local_businesses = self.client.get(
+            f"/api/{valid_latitude}/{valid_longitude}"
+        )
         self.assertEqual(get_local_businesses.status_code, status.HTTP_200_OK)
-        
+
         # Error check local businesses
-        get_local_businesses = self.client.get(f"/api/InvalidLatitude/InvalidLongitude")
-        self.assertEqual(get_local_businesses.status_code, status.HTTP_400_BAD_REQUEST)
+        get_local_businesses = self.client.get(
+            f"/api/InvalidLatitude/InvalidLongitude"
+        )
+        self.assertEqual(
+            get_local_businesses.status_code, status.HTTP_400_BAD_REQUEST
+        )

c6fbb6a4f091dc3be691598ad08cafe4ece9a44e
britney <136287986+honeepuffs@users.noreply.github.com>
2024-04-24 18:23:38 -0700

    Update third.tsx fix repositioning

--- frontend/app/(onboarding)/third.tsx
+++ frontend/app/(onboarding)/third.tsx
@@ -8,31 +8,39 @@ import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { View } from 'react-native';
 import Toast from 'react-native-toast-message';
 import { Button, Form, Separator, Text, XStack, YStack, Image } from 'tamagui';
+import { TouchableHighlight, Dimensions } from 'react-native';
 
 const Third = () => {
   const router = useRouter();
+     const width = Dimensions.get('window').width;
+     const height = Dimensions.get('window').height;
 
-  return (
-    <View>
-      <Image
-        style={{ width: 450, height: 800 }}
-        source={{
-          uri: 'https://i.ibb.co/B4mbqSp/Munch-Onboarding-03.png',
-        }}
-      />
-      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}></YStack>
-      <YStack gap={'$1'} rowGap={'$2'}>
-        <Button
-          mx={'$4'}
-          onPress={() => {
-            router.push('/login');
-          }}
-        >
-          Join Us
-        </Button>
-      </YStack>
-    </View>
-  );
-};
+     return (
+       <View flexDirection='column' flex={1}>
+
+       <Image
+           width={width}
+           height={height}
+           source={{
+             uri: 'https://i.ibb.co/B4mbqSp/Munch-Onboarding-03.png',
+           }}
+           resizeImage={'contain'}
+           position={'absolute'}
+         />
+         <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}>
+         </YStack>
+         <YStack gap={'$1'} rowGap={'$2'} paddingTop={675} flexDirection='column-reverse'>
+           <Button
+             mx={'$4'}
+             onPress={() => {
+             router.push('/login');
+           }}
+           >
+             Continue
+           </Button>
+         </YStack>
+       </View>
+     );
+   };
 
 export default Third;

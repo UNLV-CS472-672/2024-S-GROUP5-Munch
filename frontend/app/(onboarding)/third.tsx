d0ed4f1dc969178987271adccfa1e9332cc62658
honeepuffs <honeepuffs@users.noreply.github.com>
2024-04-25 01:23:55 +0000

    Apply formatting changes

--- frontend/app/(onboarding)/third.tsx
+++ frontend/app/(onboarding)/third.tsx
@@ -12,35 +12,38 @@ import { TouchableHighlight, Dimensions } from 'react-native';
 
 const Third = () => {
   const router = useRouter();
-     const width = Dimensions.get('window').width;
-     const height = Dimensions.get('window').height;
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;
 
-     return (
-       <View flexDirection='column' flex={1}>
-
-       <Image
-           width={width}
-           height={height}
-           source={{
-             uri: 'https://i.ibb.co/B4mbqSp/Munch-Onboarding-03.png',
-           }}
-           resizeImage={'contain'}
-           position={'absolute'}
-         />
-         <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}>
-         </YStack>
-         <YStack gap={'$1'} rowGap={'$2'} paddingTop={675} flexDirection='column-reverse'>
-           <Button
-             mx={'$4'}
-             onPress={() => {
-             router.push('/login');
-           }}
-           >
-             Continue
-           </Button>
-         </YStack>
-       </View>
-     );
-   };
+  return (
+    <View flexDirection='column' flex={1}>
+      <Image
+        width={width}
+        height={height}
+        source={{
+          uri: 'https://i.ibb.co/B4mbqSp/Munch-Onboarding-03.png',
+        }}
+        resizeImage={'contain'}
+        position={'absolute'}
+      />
+      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}></YStack>
+      <YStack
+        gap={'$1'}
+        rowGap={'$2'}
+        paddingTop={675}
+        flexDirection='column-reverse'
+      >
+        <Button
+          mx={'$4'}
+          onPress={() => {
+            router.push('/login');
+          }}
+        >
+          Continue
+        </Button>
+      </YStack>
+    </View>
+  );
+};
 
 export default Third;

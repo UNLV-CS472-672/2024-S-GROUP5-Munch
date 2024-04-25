83ed18f541bc5a3c43af5d73099b9011b4b82180
britney <136287986+honeepuffs@users.noreply.github.com>
2024-04-24 18:21:32 -0700

    Update first.tsx fix positioning issue

--- frontend/app/(onboarding)/first.tsx
+++ frontend/app/(onboarding)/first.tsx
@@ -1,6 +1,3 @@
-import UserInput from '@/components/UserInput';
-import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
-import { Ionicons } from '@expo/vector-icons';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'expo-router';
 import React, { useCallback, useEffect } from 'react';
@@ -8,25 +5,34 @@ import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { View } from 'react-native';
 import Toast from 'react-native-toast-message';
 import { Button, Form, Separator, Text, XStack, YStack, Image } from 'tamagui';
+import { TouchableHighlight, Dimensions } from 'react-native';
+
 
 const First = () => {
   const router = useRouter();
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;
 
   return (
-    <View>
-      <Image
-        style={{ width: 450, height: 800 }}
+    <View flexDirection='column' flex={1}>
+
+    <Image
+        width={width}
+        height={height}
         source={{
           uri: 'https://i.ibb.co/J2Fzbqp/Munch-Onboarding-01.png',
         }}
+        resizeImage={'contain'}
+        position={'absolute'}
       />
-      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}></YStack>
-      <YStack gap={'$1'} rowGap={'$2'}>
+      <YStack gap={'$2'} paddingTop={'$5'} marginHorizontal={15}>
+      </YStack>
+      <YStack gap={'$1'} rowGap={'$2'} paddingTop={675} flexDirection='column-reverse'>
         <Button
           mx={'$4'}
           onPress={() => {
-            router.push('/second');
-          }}
+          router.push('/second');
+        }}
         >
           Continue
         </Button>
